19ba7f025e093a1feb623fd5e94fc333fa8f3f2d
eniolaxo <117684350+eniolaxo@users.noreply.github.com>
2023-01-21 01:33:54 +0000

    wrote some notes in the js code to explain what i'm doing step-by-step, and attempted calculating net profit/loss

--- starter/index.js
+++ starter/index.js
@@ -90,13 +90,20 @@ var finances = [
 // calculate each of the following:
 
 // The total number of months included in the dataset
-console.log(finances.length)
+// Writing '.length' is a useful property that's used to get the number of elements in an array or a string.
+console.log('The total of months is: ' + finances.length);
+
 // The net total amount of Profit/Losses over the entire period.
+var netTotal = 0;
+for (var i = 0; i < finances.length; i++) {
+  netTotal += finances[i][1];
+}
 
 // The average of the changes in Profit/Losses over the entire period.
 
 // You will need to track what the total change in profits are from month to month and then find the average.
 // (Total/Number of months)
+
 // The greatest increase in profits (date and amount) over the entire period.
 
 // The greatest decrease in losses (date and amount) over the entire period.
